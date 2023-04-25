aae9a559dade9ce9c7b7bf06309ee01ecf73db45
임가영 <lgy0530@naver.com>
2023-04-25 21:05:39 +0900

    [REFACTOR] - 공통으로 사용하는 문자열 분리

---
+++ MyCreditManager/Common.swift
@@ -0,0 +1,13 @@
+//
+//  Common.swift
+//  MyCreditManager
+//
+//  Created by 임가영 on 2023/04/25.
+//
+
+import Foundation
+
+enum CommonString {
+    static let INVALID_MENU_INPUT = "뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해주세요."
+    static let INVALID_INPUT = "입력이 잘못되었습니다. 다시 확인해주세요."
+}
